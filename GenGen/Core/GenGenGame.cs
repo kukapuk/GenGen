5acda0a98ada6d84fc28d6bea4955f6c841dddbb
qpobedin <quanish_pobedin@rating.kz>
2026-06-18 13:35:54 +0500

    basic shit

--- GenGen/Core/GenGenGame.cs
+++ GenGen/Core/GenGenGame.cs
@@ -1,6 +1,52 @@
-﻿namespace GenGen.Core;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
-public class GenGenGame
+namespace GenGen.Core;
+
+public class GenGenGame : Game
 {
-	
+	private readonly GraphicsDeviceManager _graphics;
+	private SpriteBatch _spriteBatch = null!;
+
+	public GenGenGame()
+	{
+		_graphics = new GraphicsDeviceManager(this)
+		{
+			PreferredBackBufferWidth  = 1280,
+			PreferredBackBufferHeight = 720,
+		};
+		Content.RootDirectory = "Content";
+		Window.Title = "GenGen";
+		IsMouseVisible = true;
+	}
+
+	protected override void Initialize()
+	{
+		// инициализация систем движка
+		base.Initialize();
+	}
+
+	protected override void LoadContent()
+	{
+		_spriteBatch = new SpriteBatch(GraphicsDevice);
+		// загрузка текстур, карт и т.д.
+	}
+
+	protected override void Update(GameTime gameTime)
+	{
+		if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+			Exit();
+
+		// обновление логики
+		base.Update(gameTime);
+	}
+
+	protected override void Draw(GameTime gameTime)
+	{
+		GraphicsDevice.Clear(new Color(30, 30, 35));
+
+		// отрисовка
+		base.Draw(gameTime);
+	}
 }
--- GenGen/Program.cs
+++ GenGen/Program.cs
@@ -1,3 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using GenGen.Core;
 
-Console.WriteLine("Hello, World!");
+using var game = new GenGenGame();
+game.Run();
